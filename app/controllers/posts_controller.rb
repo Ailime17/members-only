26fbcac4a119cf04a1f0032517452e3b6bcd3ccf
Emilia <emilia.skrzypczak.es@gmail.com>
2023-05-06 22:12:48 +0200

    add #create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,13 +4,21 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @user = current_user
   end
 
   def create
-    
+    @user = current_user
+    @post = @user.posts.create(post_params)
+    redirect_to root_path
   end
 
   def index
     
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
